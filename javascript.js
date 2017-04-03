faf4ea972c8c9d4ca0c912c11f80de3d2e41531b
magdalilia-velazquez <magdalilia.velazquez@speeracademy.org>
2017-04-03 17:00:21 -0500

    made function to save last journal entry

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-//add javscript here
+function savejournalentry() 
+{
+	var journalEntry = document.getElementById(lastentry).value;
+	
+	if (journalEntry) 
+	{
+		
+	//if there is text then it will save if not then it will not save
+		localStorage.setitem("oldentry", journalEntry);
+		
+	}
+}
