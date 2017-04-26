ba8fab64bf9054986de3e4870283c3a486aae9fb
Kokayla27 <kokayla.southall@speeracademy.org>
2017-04-26 17:28:21 -0500

    Add files via upload
    ToDoList files

---
+++ todolistfun.js
@@ -0,0 +1,38 @@
+submittask(){
+    
+}
+
+$(document).ready(function() {
+    var max_fields      = 10; //maximum input boxes allowed
+    var wrapper         = $(".input_fields_wrap"); //Fields wrapper
+    var add_button      = $(".add_field_button"); //Add button ID
+    
+    var x = 1; //initlal text box count
+    $(add_button).click(function(e){ //on add input button click
+        e.preventDefault();
+        if(x < max_fields){ //max input box allowed
+            x++; //text box increment
+            $(wrapper).append('<div><input type="text" name="mytext[]"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
+        }
+    });
+    
+    $(wrapper).on("click",".remove_field", function(e){ //user click on remove text
+        e.preventDefault(); $(this).parent('div').remove(); x--;
+    })
+});
+
+function AddNewListedItem()
+{
+	var pageId = "ListedItem";
+	var element = document.getElementById(pageId);
+	var newPageText = document.getElementById("inputBox").value;
+	
+	if (newPageText)
+	{
+		element.innerHTML = newPageText;
+		localStorage.setItem(pageId, newPageText);
+	}else
+	{
+		alert("Enter some text first, please!")
+	}
+}
