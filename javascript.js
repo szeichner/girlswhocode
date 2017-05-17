87aceb3d6dc7a652862df8506dfdba2e52851835
szeichner <sarah.zeichner@gmail.com>
2017-05-17 17:29:50 -0500

    journal changes 5/17

--- javascript.js
+++ javascript.js
@@ -1,19 +1,46 @@
 //add javscript here
 function savejournalentry() 
 {
-	var journalEntry = document.getElementById("newentry").value;
-	
+    var oldEntry = localStorage.getItem("oldentry");
+    var journalEntry = document.getElementById("newentry").value;
+    if (oldEntry)
+    {
+        var entryArray = new Array(oldEntry);
+        entryArray.push(journalEntry);
+    } else {
+        var entryArray = new Array(journalEntry);
+    }
 	if (journalEntry) 
 	{
-		
 	//if there is text then it will save if not then it will not save
-		localStorage.setItem("oldentry", journalEntry);
-	    clear();	
+		localStorage.setItem("oldentry", entryArray);
+	    clearEntry();	
 	}
 }
 
-function clear()
+function clearEntry()
 {
     //to save first then clear text from input box
-    document.getElementById("newentry").value = "hello";
+    document.getElementById("newentry").value = " ";
+}
+window.onload = function()
+{
+	getOldEntry();
+}
+function getOldEntry()
+{
+	var pageId = "oldentry";
+    var firstOldEntry = localStorage.getItem("oldentry");
+    var oldEntry = firstOldEntry.split(",");
+    var text, fLen, i;
+fLen = oldEntry.length;
+text = "<ul>";
+for (i = 0; i < fLen; i++) {
+    text += "<li>" + oldEntry[i] + "</li>";
+    var mi = document.createElement("input");
+    mi.setAttribute('type', 'text');
+    mi.setAttribute('value', oldEntry[i]);
+    var form= document.getElementById("entryinbox")
+    form.appendChild(mi)
+}
 }
