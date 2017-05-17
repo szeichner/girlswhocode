3138d0ce5e3f22253c0ed906859568b9e4f41e6a
szeichner <sarah.zeichner@gmail.com>
2017-05-17 17:27:40 -0500

    updated todolist

--- todolist.js
+++ todolist.js
@@ -7,9 +7,16 @@ var addTask = function() {
 var pageRender= function() {
 	if(localStorage.getItem(0)!==null){
 		var savedTask = localStorage.getItem(0);
-		var listElement = "<li><input type='checkbox' onclick='removeChild()'>
-				<span>" + savedTask + "</span>";
-		document.getElementByTagName("ul").appendChild(listElement);
+		var listElement = document.createElement("li");
+		listElement.innerHTML="<input type='checkbox' onclick='removeMyChild()'><span>" + savedTask + "</span>";
+		document.getElementById("unorderedList").appendChild(listElement);
+		
 	}
 }
 
+
+var removeMyChild= function() {
+			var listElement= document.getElementById("unorderedList")
+
+			removeMyChild.parentNode.removeMyChild("listElement")
+}
